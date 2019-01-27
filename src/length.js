c2f74b51393976c70635655188a670da7bc1d88e
Stanley <coolstanlee@gmail.com>
2019-01-27 15:41:50 +0100

    Completed package + Babel loader

--- src/length.js
+++ src/length.js
@@ -1,13 +1,13 @@
 import { standardUnits, validUnits } from './units.js';
 
-function validateUnit(unit){
+export function validateUnit(unit){
   if (validUnits.indexOf(unit) > -1)
     return true;
   else
     return false;
 }
 
-function length(value, fromUnit, toUnit) {
+var length = (value, fromUnit, toUnit) => {
   if (!validateUnit(fromUnit) || !validateUnit(toUnit))
     return false;
 
@@ -17,7 +17,7 @@ function length(value, fromUnit, toUnit) {
 
 }
 
-// console.log( validUnits );
-console.log( length('1', "dam", "km") );
-
-export default length;
+window.c = {
+  length: length,
+  validateUnit: validateUnit,
+};
